efc57e554be98825e30a74c40b6207c018f9aee8
Brian Deery <brian@factom.org>
2015-05-30 22:40:35 -0500

    added Factom changes

--- genesis_block_generator.py
+++ genesis_block_generator.py
@@ -1,30 +1,38 @@
+#taken from https://github.com/ethereum/pyethsaletool
+#modified to generate the Factom genesis block data.
+
 import bitcoin as b
 import sys
 import json
 import os
+import blockchaininfoparser as bipar
+import binascii, re
+from datetime import datetime
 
 # Timestamp of sale start
-start = 1406066400
+start = 1427814000 # mar 31, 2015 10am central. sale start time
 # Initial sale rate
 initial_rate = 2000
 # Initial sale rate duration
-initial_period = 14 * 86400
+initial_period = 7 * 86400
 # step size for declining rate phase
-rate_decline = 30
+rate_decline = 100
 # Length of step
-rate_period = 86400
+rate_period = 7 * 86400
 # Number of declining periods
-rate_periods = 22
+rate_periods = 4
 # Final rate
-final_rate = 1337
+final_rate = 1500
 # Period during which final rate is effective
-final_period = 6 * 86400 + 3600  # 1h of slack
+final_period = 10 * 86400 + 3600  # 1h of slack
 # Accept post-sale purchases?
 post_rate = 0
 
 
-exodus = '36PrZ1KHYMpqSyAQXSG8VwbUiq2EogxLo2'
-minimum = 1000000
+exodus = '35gLt5EgB367enjSjyEDahhWWcy6p1MGf6' #factoid
+
+
+minimum = 0
 maximum = 150000000000
 
 caches = {}
@@ -53,10 +61,10 @@ def cache_method_factory(method, filename):
     return new_method
 
 # Cached versions of the BCI methods that we need
-get_block_header_data = cache_method_factory(b.get_block_header_data,
+get_block_header_data = cache_method_factory(bipar.get_block_header_data,
                                              'caches/blockheaders.json')
-fetchtx = cache_method_factory(b.fetchtx, 'caches/fetchtx.json')
-history = cache_method_factory(b.history, 'caches/history.json')
+fetchtx = cache_method_factory(bipar.fetchtx, 'caches/fetchtx.json')
+history = cache_method_factory(bipar.history, 'caches/history.json')
 
 
 # Get a dictionary of the transactions and block heights from the history
@@ -82,18 +90,17 @@ def list_purchases(obj):
         txhex = str(txs[h])
         # print txhex
         txouts = b.deserialize(txhex)['outs']
-        if len(txouts) >= 2 and txouts[0]['value'] >= minimum - 30000:
+        if len(txouts) >= 2:
             addr = b.script_to_address(txouts[0]['script'])
             if addr == exodus:
-                v = txouts[0]['value'] + 30000
+                v = txouts[0]['value'] + 10000  # add 0.1 millibit for the fees paid transfering from koinify wallet to Factom multisig
                 ht = heights[h]
                 # We care about the timestamp of the previous
                 # confirmed block before a transaction
                 t = get_block_header_data(ht - 1)['timestamp']
                 o.append({
                     "tx": h,
-                    "addr": b.b58check_to_hex(b.script_to_address(
-                                              txouts[1]['script'])),
+                    "addr": extract_pubkey(txouts[1]['script']),
                     "value": v,
                     "time": t
                 })
@@ -102,9 +109,25 @@ def list_purchases(obj):
     return o
 
 
-# Compute ether value from BTC value
+def extract_pubkey(script):
+    if re.match('^[0-9a-fA-F]*$', script):
+        script = binascii.unhexlify(script)
+    if script[:10] != 'j(FACTOM00':
+        return 0  # this is an error
+    else:
+        pubkey = script[10:]
+        if len (pubkey) != 32:
+            return 0  # this is an error
+        else:
+            return pubkey.encode("hex")
+
+# Compute factoid value from BTC value
 def evaluate_purchases(purchases):
-    o = {}
+    index = 0
+
+    f1=open('./genesis.csv', 'w')
+    f1.write('funding txid,conf time unix,conf time human,ed25519 pubkey,# bitcoins,rate,# factoshis\n')
+
     for p in purchases:
         if p["time"] < start + initial_period:
             rate = initial_rate
@@ -116,11 +139,25 @@ def evaluate_purchases(purchases):
             rate = final_rate
         else:
             rate = post_rate
-        # Round to the nearest finney (nearest ether would be // 10**8)
-        o[p["addr"]] = {
-            "wei": str((p["value"] * rate // 10**5) * 10**15)
-        }
-    return o
+
+        f1.write(p['tx'])
+        f1.write(',')
+        f1.write(str(p['time']))
+        f1.write(',')
+        f1.write(datetime.fromtimestamp(p['time']).strftime('%Y-%m-%d %H:%M:%S'))
+        f1.write(',')
+        f1.write(str(p["addr"]))
+        f1.write(',')
+        f1.write(str(p["value"]))
+        f1.write(',')
+        f1.write(str(rate))
+        f1.write(',')
+        f1.write(str(p["value"] * rate))
+        f1.write('\n')
+
+        index += 1
+
+    return "wrote genesis.csv"
 
 
 def evaluate():
