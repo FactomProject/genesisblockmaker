4c6bfe82cc19b97939287b261176b820be340cd0
Brian Deery <brian@factom.org>
2015-08-31 01:02:49 -0500

    added EC key generation

--- serverKey/makePrivateKey.py
+++ serverKey/makePrivateKey.py
@@ -12,7 +12,10 @@ import os
 import ed25519djb as ed25519
 import re
 import sys
+import base58pkg as base58
+import hashlib
 
+ec_public_key_prefix = "592a"
 
 def doublecheck_key_works(privkey, pubkey):
     """
@@ -23,16 +26,47 @@ def doublecheck_key_works(privkey, pubkey):
     message = "message to sign"
     signature = ed25519.signature(message, privkey, pubkey)
 
-    print "test sig: " + signature.encode('hex')
+    #print "test sig: " + signature.encode('hex')
     #signaturebad = signature[:1] + 'X' + signature[2:]
     #signaturebad2 = signature[:-2] + 'X' + signature[-1:]
     ed25519.checkvalid(signature, message, pubkey)
     # if this script got this far, then the signature validated.
-    print "signature good"
+    #print "signature good"
 
+def public_EC_key_to_human(public_key, advanced):
+    """
+    this function takes a 32 byte ed25519 public key in binary form and
+    returns a string with a human readable public key according to this spec:
+    https://github.com/FactomProject/FactomDocs/blob/master/factomDataStructureDetails.md#entry-credit-address
+    For example.  a binary public key passed like this :
+    0000000000000000000000000000000000000000000000000000000000000000
+    would return this string:
+    EC1m9mouvUQeEidmqpUYpYtXg8fvTYi6GNHaKg8KMLbdMBrFfmUa
+    
+    The advanced parameter set to true displays technical internal data
+    """
+
+    hex_seed = public_key.encode('hex')
+    public_key_prefix = ec_public_key_prefix + hex_seed
+
+    if advanced == True:
+        print "public key with prefix:   " + public_key_prefix
+
+    digest = hashlib.sha256(hashlib.sha256(public_key_prefix.decode("hex")).digest()).digest()
+    if advanced == True:
+        print "public key hash: " + digest.encode('hex')
+    checksummed_public_key = public_key_prefix + digest[:4].encode('hex')
+    if advanced == True:
+        print "public key with checksum: " + checksummed_public_key
+    human_pubkey =  base58.b58encode(checksummed_public_key.decode("hex"))
+    if advanced == True:
+        print "Human readable public key: " + human_pubkey
+    return human_pubkey
 
 
 def main():
+
+
 
 
     print "Server Private Key Generator"
@@ -43,10 +77,22 @@ def main():
     pubkey = ed25519.publickey(private_key)
     doublecheck_key_works(private_key, pubkey)
 
-    print "\n \n Public key for common/constants.go SERVER_PUB_KEY:"
+    print "\n Public key for common/constants.go SERVER_PUB_KEY:"
     print pubkey.encode('hex')
     print "\n Private key for ~/.factom/factom.conf :"
     print private_key.encode('hex') + pubkey.encode('hex')
+    
+    print "\n\n\n"
+    
+    private_key = os.urandom(32)
+    pubkey = ed25519.publickey(private_key)
+    doublecheck_key_works(private_key, pubkey)
+
+    print "\n Private EC key for ~/.factom/factom.conf :"
+    print private_key.encode('hex') + pubkey.encode('hex')
+    print "\n Public Human readable EC address to fund :"
+    print public_EC_key_to_human(private_key, False)
+    
 
 if "__main__" == __name__:
         main()
